01c705150f0fe4d413f5f5034889ee07de4ce48c
羡笙z <14679730+xiansheng-z@user.noreply.gitee.com>
2024-12-24 01:00:45 +0800

    整活

---
+++ js/detect.js
@@ -0,0 +1,14 @@
+function isMobileBrowser() {
+    // 使用正则表达式匹配用户代理字符串
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|symbian|windows phone|tablet|mobile/i.test(userAgent);
+  }
+  
+  // 调用函数检查是否是手机浏览器
+  if (isMobileBrowser()) {
+    // 如果是手机浏览器，则跳转到手机网站
+    window.location.href = '../html/detect.html';
+  } else {
+    // 如果不是手机浏览器，则跳转到PC网站
+    window.location.href = '../index.html';
+  }
