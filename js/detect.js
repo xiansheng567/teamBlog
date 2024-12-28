7571e29bca64c5dc5dc16142c974c1680bae863e
羡笙z <14679730+xiansheng-z@user.noreply.gitee.com>
2024-12-28 22:36:08 +0800

    final3.0

--- js/detect.js
+++ js/detect.js
@@ -4,9 +4,8 @@ function isMobileBrowser() {
     return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|symbian|windows phone|tablet|mobile/i.test(userAgent);
   }
   
-  // 调用函数检查是否是手机浏览器
+  
   if (isMobileBrowser()) {
-    // 如果是手机浏览器，则跳转到手机网站
-    window.location.href = "../";
+    window.location.href = "../html/detect.html";
   } else {
   }
